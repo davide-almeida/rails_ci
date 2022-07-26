a22aef34ebaf4d90cb0872db6d88ecba11a405d0
Davide Almeida <davide.jhonatan@gmail.com>
2022-07-26 16:40:36 -0300

    feat: add simplecov gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter 'channels'
+  add_filter 'helpers'
+  add_filter 'jobs'
+  add_filter 'mailers'
+  add_filter 'serializers'
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
